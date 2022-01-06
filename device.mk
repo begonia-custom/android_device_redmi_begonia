a482fefa45ca0adbd04109b5e5d7af083bf7b4ba
TTTT555 <albukerko@gmail.com>
2022-01-06 21:14:10 +0000

    [SQUASH] Build xiaomiparts with additions * kang app by https://github.com/StagOS-Devices/device_redmi_begonia * drop KCAL * replace miSound effects with Dirac sound effects * make performance profiles * fix vibration volumes * add settings daemon * add sepolicy denies * also adapt to A12 ROMs

--- device.mk
+++ device.mk
@@ -252,6 +252,7 @@ PRODUCT_PACKAGES += \
     partition_permission.sh \
     setup_MTK_In-Call_volume_adjust.sh \
     throttle.sh \
+    xiaomiparts.sh \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6785.rc \
@@ -260,6 +261,7 @@ PRODUCT_PACKAGES += \
     init.project.rc \
     init.sensor_1_0.rc \
     init.ttl.rc \
+    init.xiaomi_parts.rc \
     fstab.mt6785 \
     fstab.mt6785_ramdisk \
     ueventd.mt6785.rc
@@ -402,6 +404,10 @@ PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
     PresencePolling \
     RcsService
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
 
 # Inherit vendor
 $(call inherit-product, vendor/redmi/begonia/begonia-vendor.mk)
--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -22,6 +22,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
 $(call inherit-product, vendor/v4afx/config.mk)
+$(call inherit-product, vendor/dirac/config.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
